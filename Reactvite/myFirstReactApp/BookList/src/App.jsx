90528d7fc64d66d05d94c40d083933463062ff2e
tecmalik <abdulmalikojo2@gmail.com>
2025-05-01 16:57:23 +0100

    updating Book list

--- Reactvite/myFirstReactApp/BookList/src/App.jsx
+++ Reactvite/myFirstReactApp/BookList/src/App.jsx
@@ -1,17 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import './App.css'
 import BookList from "./components/BookList"
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     
-      <>
+    <>
       <BookList/>
-      </>
+    </>
   )
 }
 
--- Reactvite/myFirstReactApp/BookList/src/components/BookList.jsx
+++ Reactvite/myFirstReactApp/BookList/src/components/BookList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import style from "./bookList.module.css"
 
 
@@ -8,15 +9,40 @@ const BookList = () =>{
         "The Wise Man's Fear",
         "Kafka on the Shore",
         "The Master and the Margarita"
-    ]
-    
+    ];
 
-    const deleteHandler = (item)=>{
-        // console.log(event)
-        // bookList.filter((book) => book !== item)
-    }
+    const [books, setBooks] = useState(bookList);
+    const[newBook, setNewBook] = useState();
+    console.log(books)
+    console.log(newBook)
+
+   const deleteHandler = (item)=>{
+        setBooks(books.filter((book) => book !== item))
+        }
+
+        const collectBookTitle = (e) => {
+            setNewBook(e.target.value.trim())
+
+        }
+        const handleSubmit =(e)=>{ 
+            e.preventDefault();
+            if(newBook){
+                setBooks(prevBook=>[...prevBook, newBook])
+
+            }
+        }
+
+        const handleFilterBooks =(e)=>{
+            e.preventDefault();
+            let setSearchBookValue;
+            setSearchBookValue = e.target.value.toLowerCase();
+            setSearchBookValue(searchValue)
+            
+        }
+        const filteredBooks = books.filter((book)=>book.toLowerCase().includes());
 
 return(
+
     <div id= {style.wrapper}>
     <header>
         <div id = {style.pageBanner}>
@@ -24,36 +50,34 @@ return(
             
       <p>Books</p>
       <form id={style.searchBooks}>
-        <input type="text" placeholder="Search books..." />
+        <input type="text" placeholder="Search books..." onKeyUp={handleFilterBooks}/>
       </form>
         </div>
     </header>
     <div id={style.booklist}>
         <h2 className= {style.title}>Books to Read</h2>
         <ul>
-            { bookList.map((book,index)=>(
+            { books.map((book,index)=>(
 
-            <li key = {index}>
-                <span className={style.name}></span>
-                <span className={style.delete} onClick={ ()=> deleteHandler(book)}>delete</span>
-            </li>
+                <li key = {index}>
+                    <span className={style.name}>{book}</span>
+                    <span className={style.delete} onClick={ ()=> deleteHandler(book)}>delete</span>
 
-            ))
+                </li>
+
+                ))
             }
-           
-
-        </ul>
+        
+           </ul>
     </div>
-    <form id={style.addBook}>
-        <input type="text" placeholder="Add a book..." />
+    <form onSummit = {handleSubmit}  id={style.addBook}>
+        <input type="text" placeholder="Add a book..." onChange = {collectBookTitle} /> 
         <button>Add</button>
     </form>
 
 </div>
 
-)
-
-}
+)}
 
 
 export default BookList;
